39fa6ebc10fff5968b12689a5b5b645b0a87210f
Samir Neupane <sasuki984@gmail.com>
2025-01-19 14:27:33 +0545

    intialize project #1

---
+++ discord_bot/bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context  # type: ignore
+from env import ENV
+
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot is ready. Logged in as {bot.user}.")
+
+
+@bot.command()
+async def hello(ctx: Context):  # type: ignore
+    await ctx.send("Hello, I am journal Bot.")
+
+
+def main():
+    # Set up bot intents
+    intents.messages = True
+    intents.message_content = True
+    bot.run(ENV.DISCORT_BOT_TOKEN)
+
+
+if __name__ == "__main__":
+    main()
