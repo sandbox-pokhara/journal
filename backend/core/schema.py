49130fbf9e874981b428f00f01781c7daa54eeb3
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2025-02-02 16:05:05 +0545

    refactor: remove ninja API and docker compose swarm

--- backend/core/schema.py
+++
@@ -1,9 +0,0 @@
-from ninja import Schema
-
-
-class CheckInSchema(Schema):
-    nickname: str
-
-
-class GenericSchema(Schema):
-    detail: str
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -18,9 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.api import api
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", api.urls),
 ]
