d577265e81b574d797477e8f76eefb67a8578f00
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2025-02-03 15:47:23 +0545

    fix: fix logic in create_absence

--- backend/discord_bot/commands/absences.py
+++ backend/discord_bot/commands/absences.py
@@ -8,29 +8,33 @@ from project.env import ENV
 
 
 async def create_absence(user: User, message: discord.Message):
-
-    now = timezone.now()
-    start_of_month = now.replace(day=1)
-    total_absences = (
-        await Absence.objects.filter(
-            user=user, date_created__gt=start_of_month
-        ).aaggregate(Sum("days"))
-    )["days__sum"] or 0
-
-    if total_absences >= ENV.ABSENCES_ALLOWED_PER_MONTH:
-        return await message.channel.send(
-            embed=discord.Embed(
-                title="❌ Oops!",
-                description=(
-                    "You have exceeded your permitted number of absences"
-                    f" ({ENV.ABSENCES_ALLOWED_PER_MONTH}) for this month."
-                ),
-                color=discord.Color.orange(),
-            )
-        )
-
     is_break = "break" in message.content.lower()
     is_partial = "partial" in message.content.lower()
+
+    if not is_break and not is_partial:
+        now = timezone.now()
+        start_of_month = now.replace(day=1)
+        total_absences = (
+            await Absence.objects.filter(
+                is_partial=False,
+                is_break=False,
+                user=user,
+                date_created__gt=start_of_month,
+            ).aaggregate(Sum("days"))
+        )["days__sum"] or 0
+
+        if total_absences >= ENV.ABSENCES_ALLOWED_PER_MONTH:
+            return await message.channel.send(
+                embed=discord.Embed(
+                    title="❌ Oops!",
+                    description=(
+                        "You have exceeded your permitted number of absences"
+                        f" ({ENV.ABSENCES_ALLOWED_PER_MONTH}) for this month."
+                    ),
+                    color=discord.Color.orange(),
+                )
+            )
+
     absence = await Absence.objects.acreate(
         user=user,
         message=message.content,
