f138487f7056cde597bc70f78a29bb111e0064e9
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2025-02-12 23:55:51 +0545

    refactor: update summary formatting

--- backend/discord_bot/commands/summary.py
+++ backend/discord_bot/commands/summary.py
@@ -1,11 +1,8 @@
-from typing import Any
-
 import discord
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.timezone import localdate
 from django.utils.timezone import timedelta
-from tabulate import tabulate
 
 from core.models import Absence
 from core.models import CheckIn
@@ -13,39 +10,10 @@ from core.models import Holiday
 
 
 async def summary(user: User, message: discord.Message):
-    # upcoming holiday
-    today = timezone.localtime(timezone.now()).date()
-
-    holidays = Holiday.objects.filter(
-        date__gte=today, date__lt=today + timedelta(days=30)
-    )
-    content = "\n".join([f"{h.date}: {h.description}" async for h in holidays])
-
-    upcoming_holiday_embed = discord.Embed(
-        title="🎉 Upcoming Holidays!",
-        description=content,
-        color=discord.Color.green(),
-    )
-
-    # list absence
-    absences = Absence.objects.filter(user=user)
-    if await absences.acount() <= 0:
-        content = "No absences found for the user."
-    else:
-        content = "\n".join(
-            [
-                f"{a.date_created.date()}, {a.days} day(s), {a.message}"
-                async for a in absences
-            ]
-        )
-    list_absence_embed = discord.Embed(
-        title=f"🎉 Absences for User {user.username}!",
-        description=content,
-        color=discord.Color.green(),
-    )
+    output = ""
 
     # attendence table
-    table: list[list[Any]] = []
+    table: list[str] = []
     async for u in User.objects.all():
         total_days = (
             localdate(timezone.now()) - localdate(u.date_joined)
@@ -57,16 +25,42 @@ async def summary(user: User, message: discord.Message):
         check_ins = await CheckIn.objects.filter(user=u).acount()
         absences = await Absence.objects.filter(user=u).acount()
         days_to_cover = total_days - holidays_count - check_ins - absences
+        day = "day" if days_to_cover == 1 else "days"
+        table.append(f"{u.username} has {days_to_cover} {day} to cover.")
+    table_str = "\n".join(table)
+    output += f"**Attendance:**\n{table_str}\n\n"
 
-        table.append([u.username, f"{check_ins}/{absences}/{days_to_cover}"])
-    attendance_table = tabulate(
-        table,
-        headers=["username", "result"],
-        tablefmt="rounded_outline",
+    # upcoming holiday
+    today = timezone.localtime(timezone.now()).date()
+
+    holidays = Holiday.objects.filter(
+        date__gte=today, date__lt=today + timedelta(days=30)
     )
-    attendance_info = "\nresult = check-ins/absences/days-to-cover\n"
+    if not await holidays.aexists():
+        content = "No results."
+    else:
+        content = "\n".join(
+            [f"{h.date}: {h.description}" async for h in holidays]
+        )
+    output += f"**Upcoming holidays:**\n{content}\n\n"
+
+    # list absence
+    absences = Absence.objects.filter(user=user)
+    if await absences.acount() <= 0:
+        content = "No results."
+    else:
+        content = "\n".join(
+            [
+                f"{a.date_created.date()}, {a.days} day(s), {a.message}"
+                async for a in absences
+            ]
+        )
+    output += f"**Absences for user {user.username}:**\n{content}\n\n"
 
     await message.channel.send(
-        f"```{attendance_table}{attendance_info}```",
-        embeds=[upcoming_holiday_embed, list_absence_embed],
+        embed=discord.Embed(
+            title=f"🎉 Summary",
+            description=output,
+            color=discord.Color.green(),
+        )
     )
