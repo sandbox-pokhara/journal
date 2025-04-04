2ae38ec58d663e82eebfcef8021e586b4d26529f
Samir Neupane <sasuki984@gmail.com>
2025-04-04 16:17:20 +0545

    fix: add timezone and enforce single checkin

--- backend/discord_bot/commands/check_ins.py
+++ backend/discord_bot/commands/check_ins.py
@@ -1,16 +1,28 @@
 import discord
+import pytz
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.utils.timezone import timedelta
 
 from core.models import CheckIn
 from core.models import Message
+from core.models import UserDetail
 
 
 async def create_check_in(user: User, message: discord.Message):
-    eight_hours_ago = timezone.now() - timedelta(hours=8)
+    DEFAULT_TZ = "Asia/Kathmandu"
+    try:
+        user_detail = await UserDetail.objects.aget(user=user)
+        user_tz = user_detail.timezone
+    except UserDetail.DoesNotExist:
+        user_tz = DEFAULT_TZ
+
+    tz = pytz.timezone(user_tz)
+    utc_now = timezone.now()
+    local_time = utc_now.astimezone(tz)
+    today_date = local_time.date()
+
     checkin_count = await CheckIn.objects.filter(
-        user=user, date_created__gte=eight_hours_ago
+        user=user, date_created__date=today_date
     ).acount()
     if checkin_count >= 1:
         return await message.channel.send(
@@ -22,7 +34,7 @@ async def create_check_in(user: User, message: discord.Message):
         )
 
     check_in = await CheckIn.objects.acreate(
-        user=user, message=message.content
+        user=user, message=message.content, date_created=local_time
     )
     await Message.objects.acreate(id=message.id, check_in=check_in)
 
