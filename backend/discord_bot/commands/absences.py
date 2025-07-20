027ab22d513cc6966f0df76c25800d317bf4f8ee
callback cat <76839908+chamanbravo@users.noreply.github.com>
2025-07-20 13:53:55 +0545

    refactor: add webhook for all messages

--- backend/discord_bot/commands/absences.py
+++ backend/discord_bot/commands/absences.py
@@ -1,9 +1,10 @@
 import discord
+import httpx
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.utils import timezone
 
-from core.models import Absence, Message
+from core.models import Absence, Message, Webhook
 from project.env import ENV
 
 
@@ -32,4 +33,41 @@ async def create_absence(user: User, message: discord.Message):
         is_paid=True,
     )
     await Message.objects.acreate(id=message.id, absence=absence)
+
+    webhooks: list[Webhook] = []
+    async for webhook in Webhook.objects.all():
+        webhooks.append(webhook)
+
+    if webhooks:
+        payload = {
+            "username": "Sandbox Journal",
+            "content": f"absence - {user.username} - {message.content}",
+        }
+        headers = {"Content-Type": "application/json"}
+
+        async with httpx.AsyncClient() as client:
+            for webhook in webhooks:
+                try:
+                    response = await client.post(
+                        webhook.webhook_url, json=payload, headers=headers
+                    )
+                    response.raise_for_status()
+                except httpx.HTTPStatusError as e:
+                    return await message.channel.send(
+                        embed=discord.Embed(
+                            title="❌ Oops!",
+                            description=(
+                                "Failed to send webhook:"
+                                f" {e.response.status_code}, {e.response.text}"
+                            ),
+                        )
+                    )
+                except httpx.RequestError as e:
+                    return await message.channel.send(
+                        embed=discord.Embed(
+                            title="❌ Oops!",
+                            description=f"An error occurred while requesting {e}.",
+                        )
+                    )
+
     return await message.add_reaction("🫡")
